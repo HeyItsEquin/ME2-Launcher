e04564e985338ff1292fb51da99606202c819091
HeyItsEquin <theoriginalthemor@gmail.com>
2025-09-10 03:12:26 -0700

    Added numerous files. Added navigation view as well as different pages.

---
+++ ME2Launcher/Views/Controls/MainContent.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using ME2Launcher.Views.Pages;
+
+namespace ME2Launcher.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for MainContent.xaml
+    /// </summary>
+    public partial class MainContent : UserControl
+    {
+        public MainContent()
+        {
+            InitializeComponent();
+            Loaded += MainContent_Loaded;
+        }
+
+        private void MainContent_Loaded(object sender, RoutedEventArgs e)
+        {
+            RootNav.Navigate(typeof(HomePage));
+        }
+    }
+}
